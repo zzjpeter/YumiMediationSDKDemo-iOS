c60e527e7bdb28eaa114b33b7b7b3d4f40ff9aec
ShunZhi Tang <tangshunzhi@123juzi.com>
2017-07-13 16:25:17 +0800

    Add YumiMobiAppViewController

---
+++ YumiMediationSDKDemo/YumiMobiAppViewController.m
@@ -0,0 +1,37 @@
+//
+//  YumiMobiAppViewController.m
+//  YumiMediationSDKDemo
+//
+//  Created by ShunZhi Tang on 2017/7/13.
+//  Copyright © 2017年 Zplay. All rights reserved.
+//
+
+#import "YumiMobiAppViewController.h"
+
+@interface YumiMobiAppViewController ()
+
+@end
+
+@implementation YumiMobiAppViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
