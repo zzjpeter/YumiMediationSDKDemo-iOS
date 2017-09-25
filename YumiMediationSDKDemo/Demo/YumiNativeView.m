11d07c65499ad2a5ffbac92af242401981ae4c5f
wangzeyong <wzy2010416033@163.com>
2017-09-25 16:18:10 +0800

    add native example

---
+++ YumiMediationSDKDemo/Demo/YumiNativeView.m
@@ -0,0 +1,13 @@
+//
+//  YumiNativeView.m
+//  YumiMediationSDK-iOS
+//
+//  Created by 王泽永 on 2017/9/22.
+//  Copyright © 2017年 JiaDingYi. All rights reserved.
+//
+
+#import "YumiNativeView.h"
+
+@implementation YumiNativeView
+
+@end
