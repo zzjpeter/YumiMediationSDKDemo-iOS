acc6d646416a4867c8cc7a4353fb7252009fcfb1
ShunZhi Tang <tangshunzhi@zplay.com>
2017-12-20 11:57:58 +0800

    Update placementID and ironSource version

--- YumiMediationSDKDemo/Demo/YumiViewController.h
+++ YumiMediationSDKDemo/Demo/YumiViewController.h
@@ -8,7 +8,7 @@
 
 @import UIKit;
 
-static NSString *const placementID = @"p9l3j882";
+static NSString *const placementID = @"pwzcvso3";
 static NSString *const channelID = @"";
 static NSString *const versionID = @"";
 
