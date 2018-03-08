7179889b23239435fda31f365e9340ad42b76cf4
tangfeng <dreamtangf@163.com>
2018-03-08 11:28:11 +0800

    Add  zplay distribute

--- fastlane/decrypt_p12.sh
+++ fastlane/decrypt_p12.sh
@@ -2,5 +2,7 @@
 if [ -z "$ENCRYPTION_PASSWORD" ]; then
   echo "no password, stop here..."
 else
-  openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in fastlane/Certificates/distribution.p12.enc -d -a -out fastlane/Certificates/distribution.p12
+  openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in fastlane/Certificates/ZplayAds_Distribute.p12.enc -d -a -out fastlane/Certificates/ZplayAds_Distribute.p12
+  mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+  openssl aes-256-cbc -k $ENCRYPTION_PASSWORD -in fastlane/Certificates/ZplayAds_Distribute.mobileprovision.enc -d -a -out ~/Library/MobileDevice/Provisioning\ Profiles/ZplayAds_Distribute.mobileprovision
 fi
